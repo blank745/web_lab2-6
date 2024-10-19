0be0dbb974f48b3100a54c92f4d6cf080a649766
Freebeing <74550408+blank745@users.noreply.github.com>
2024-10-19 15:37:02 +0300

    Лаб. 4 с корректировками (сдано)

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,11 @@ let main_dish_sum = 0;
 let drink_sum = 0;
 let sum = 0;
 
+
+
 function select_dish(name, id, price) {
+
+
     let soup = document.getElementById('soup_p');
     let main_dish = document.getElementById('main_dish_p');
     let drink = document.getElementById('drink_p');
@@ -39,12 +43,12 @@ let soup_block = document.getElementById('soup')
 let div_blocks = ''
 soup_array.forEach(dish => {
     div_blocks += `
-                    <div onclick="select_dish('${dish.category}', ${soup_array.indexOf(dish)+1}, ${dish.price})" data-dish="${dish.keyword}">
+                    <div name="food_block" onclick="select_dish('${dish.category}', ${soup_array.indexOf(dish)+1}, ${dish.price});" data-dish="${dish.keyword}">
                     <img src="${dish.image}" alt="soup">
                     <p class="price">${dish.price} рублей</p>
                     <p class="name">${dish.name}</p>
                     <p class="mass">${dish.count}</p>
-                    <button>Добавить</button>
+                    <button name="food_button">Добавить</button>
                     </div>
     `
 });
@@ -59,12 +63,12 @@ let main_dish_block = document.getElementById('main_dish')
 div_blocks = ''
 dishes_array.forEach(dish => {
     div_blocks += `
-                    <div onclick="select_dish('${dish.category}', ${dishes_array.indexOf(dish)+1}, ${dish.price})" data-dish="${dish.keyword}">
+                    <div name="food_block" onclick="select_dish('${dish.category}', ${dishes_array.indexOf(dish)+1}, ${dish.price})" data-dish="${dish.keyword}">
                     <img src="${dish.image}" alt="soup">
                     <p class="price">${dish.price} рублей</p>
                     <p class="name">${dish.name}</p>
                     <p class="mass">${dish.count}</p>
-                    <button>Добавить</button>
+                    <button name="food_button">Добавить</button>
                     </div>
     `
 });
@@ -79,12 +83,12 @@ let drink_block = document.getElementById('drink')
 div_blocks = ''
 drinks_array.forEach(dish => {
     div_blocks += `
-                    <div onclick="select_dish('${dish.category}', ${drinks_array.indexOf(dish)+1}, ${dish.price})" data-dish="${dish.keyword}">
+                    <div name="food_block" onclick="select_dish('${dish.category}', ${drinks_array.indexOf(dish)+1}, ${dish.price})" data-dish="${dish.keyword}">
                     <img src="${dish.image}" alt="soup">
                     <p class="price">${dish.price} рублей</p>
                     <p class="name">${dish.name}</p>
                     <p class="mass">${dish.count}</p>
-                    <button>Добавить</button>
+                    <button name="food_button">Добавить</button>
                     </div>
     `
 });
@@ -94,6 +98,8 @@ drink_block.innerHTML = `
     ${div_blocks}
     </div>
 `
+
+
 
 window.onload = function() {
     let time_radio_buttons = document.getElementsByName('delivery-time-type');
@@ -119,6 +125,42 @@ window.onload = function() {
         radio.addEventListener('click', check_as_fast)
     });
 
+
+    function reset_order() {
+        let soup_p = document.getElementById('soup_p');
+        let main_dish_p = document.getElementById('main_dish_p');
+        let drink_p = document.getElementById('drink_p');
+
+        soup_p.innerHTML = "-- Выберите суп --";
+        main_dish_p.innerHTML = "-- Выберите главное блюдо --";
+        drink_p.innerHTML = "-- Выберите напиток --";
+
+        document.getElementById('order_h2').innerHTML = 'Ваш заказ';
+
+        document.getElementById('nothing_is_chosen').style.display = 'block';
+        document.getElementById('order_inputs').style.display = 'none';
+
+        soup_sum = 0;
+        main_dish_sum = 0;
+        drink_sum = 0;
+        sum = 0;
+    }
+    let reset_button = document.getElementById('reset');
+    reset_button.addEventListener('click', reset_order)
+
+    let div_dishes = document.getElementsByName('food_block');
+    div_dishes.forEach(block => {
+        block.addEventListener('click', show_order_inp)
+    })
+
+}
+
+function show_order_inp() {
+    let nothing = document.getElementById('nothing_is_chosen');
+    let order_inps = document.getElementById('order_inputs');
+
+    nothing.style.display = 'none';
+    order_inps.style.display = 'block';
 }
 
 
